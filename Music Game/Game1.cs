0cde36f33fabc243d18739125ea2865a9925e4ec
xiaoxigua <aijdfajodwsdf@gmail.com>
2021-03-19 20:04:29 +0800

    Add Game Scenes

--- Music Game/Game1.cs
+++ Music Game/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 
 namespace Music_Game
 {
@@ -14,7 +15,7 @@ namespace Music_Game
         SpriteFont font;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        private Game_Scenes currentScene;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -24,8 +25,10 @@ namespace Music_Game
             IsMouseVisible = true;
         }
 
+
         protected override void Initialize()
         {
+            
             // TODO: Add your initialization logic here
             _graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width - 2;
             _graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height - 2;
@@ -34,19 +37,19 @@ namespace Music_Game
             //_graphics.IsFullScreen = true;
 
             _graphics.ApplyChanges();
-            ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
-                _graphics.PreferredBackBufferHeight / 2);
-            FPS = new Vector2(10, 10);
-            ballSpeed = 100f;
-
+            //ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+            //    _graphics.PreferredBackBufferHeight / 2);
+            //FPS = new Vector2(10, 10);
+            //ballSpeed = 100f;
+            currentScene = new Start(_graphics, Content);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            ballTexture = Content.Load<Texture2D>("ball");
-            font = Content.Load<SpriteFont>("File");
+            //ballTexture = Content.Load<Texture2D>("ball");
+            //font = Content.Load<SpriteFont>("File");
             // TODO: use this.Content to load your game content here
         }
 
@@ -54,21 +57,11 @@ namespace Music_Game
         {
 //            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
 //                Exit();
-            var kstate = Keyboard.GetState();
-
-            if (kstate.IsKeyDown(Keys.Up))
-                ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            if (kstate.IsKeyDown(Keys.Down))
-                ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            if (kstate.IsKeyDown(Keys.Left))
-                ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            if (kstate.IsKeyDown(Keys.Right))
-                ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            // TODO: Add your update logic here
-
+            Game_Scenes? Scene =  currentScene.Update(gameTime);
+            if (Scene != null)
+            {
+                this.currentScene = Scene;
+            }
             base.Update(gameTime);
         }
 
@@ -76,20 +69,20 @@ namespace Music_Game
         {
             GraphicsDevice.Clear(new Color(30, 30, 30));
             _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
-
-            _spriteBatch.Draw(
-                ballTexture,
-                ballPosition,
-                null,
-                new Color(255,255,255,0.5f),
-                0f,
-                new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
-                Vector2.One,
-                SpriteEffects.None,
-                0f
-            );
+            currentScene.Draw(_spriteBatch, gameTime);
+            //_spriteBatch.Draw(
+            //    ballTexture,
+            //    ballPosition,
+            //    null,
+            //    new Color(255,255,255,0.5f),
+            //    0f,
+            //    new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+            //    Vector2.One,
+            //    SpriteEffects.None,
+            //    0f
+            //);
             
-            _spriteBatch.DrawString(font, ((int)Math.Ceiling(1000 / (float)gameTime.ElapsedGameTime.TotalMilliseconds)).ToString(), FPS, Color.White);
+            //_spriteBatch.DrawString(font, ((int)Math.Ceiling(1000 / (float)gameTime.ElapsedGameTime.TotalMilliseconds)).ToString(), FPS, Color.White);
             _spriteBatch.End();
             
             // TODO: Add your drawing code here
