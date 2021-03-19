0b69121211526abece520cf93329601430040046
xiaoxigua <aijdfajodwsdf@gmail.com>
2021-03-20 01:26:35 +0800

    Add start

--- Music Game/Games/Start.cs
+++ Music Game/Games/Start.cs
@@ -15,7 +15,7 @@ namespace Music_Game
         private Texture2D ballTexture;
         private Vector2 ballPosition;
         private Vector2 FPS;
-        private float ballSpeed;
+        private float alpha;
         private SpriteFont font;
         public Start(GraphicsDeviceManager _graphics, ContentManager Content)
         {
@@ -24,48 +24,48 @@ namespace Music_Game
             ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
                 _graphics.PreferredBackBufferHeight / 2);
             FPS = new Vector2(10, 10);
-            ballSpeed = 100f;
+            alpha = 1;
             ballTexture = Content.Load<Texture2D>("ball");
             font = Content.Load<SpriteFont>("File");
         }
 
         public override Game_Scenes Update(GameTime gameTime)
         {
-            if (gameTime.TotalGameTime.Seconds > 30)
+            if (alpha < 0)
             {
                 return new Test(_graphics, _content);
             }
             var kstate = Keyboard.GetState();
+            alpha -= (float)0.5 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            //if (kstate.IsKeyDown(Keys.Up))
+            //    ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            if (kstate.IsKeyDown(Keys.Up))
-                ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            //if (kstate.IsKeyDown(Keys.Down))
+            //    ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            if (kstate.IsKeyDown(Keys.Down))
-                ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            //if (kstate.IsKeyDown(Keys.Left))
+            //    ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            if (kstate.IsKeyDown(Keys.Left))
-                ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            if (kstate.IsKeyDown(Keys.Right))
-                ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            //if (kstate.IsKeyDown(Keys.Right))
+            //    ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             return null;
         }
 
         public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
-            spriteBatch.Draw(
-                ballTexture,
-                ballPosition,
-                null,
-                new Color(255, 255, 255, 0.5f),
-                0f,
-                new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
-                Vector2.One,
-                SpriteEffects.None,
-                0f
-            );
+            //spriteBatch.Draw(
+            //    ballTexture,
+            //    ballPosition,
+            //    null,
+            //    Color.White*alpha,
+            //    0f,
+            //    new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+            //    Vector2.One,
+            //    SpriteEffects.None,
+            //    0f
+            //);
 
-            spriteBatch.DrawString(font, (gameTime.TotalGameTime.Seconds).ToString(), FPS, Color.White);
+            spriteBatch.DrawString(font, "XYZ Studio ", ballPosition, Color.White * alpha);
         }
     }
 }
